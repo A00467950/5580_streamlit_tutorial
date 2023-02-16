fda2b17d68bd90b56c1f8a1d488f1aca07ee6210
Samuel Thiankee <thiankee.samuel@gmail.com>
2023-02-15 20:15:23 -0400

    Fix typo.

--- main.py
+++ main.py
@@ -17,6 +17,6 @@ pe_l = st.slider('petal length (cm)', min_value=0, max_value=10)
 pe_w = st.slider('petal width (cm)', min_value=0, max_value=10)
 
 
-prediction = clj.predict([[sp_l, sp_w, pe_l, pe_w]])
+prediction = clf.predict([[sp_l, sp_w, pe_l, pe_w]])
 
 st.write(LABELS[predictions[0]])
